5516ffa186a3edf56fde0931524cc968952bf863
=Paivi <paivi.e.suomela@gmail.com>
2016-10-08 12:19:18 +0100

    updated to django 1.10

--- random_greetings/urls.py
+++ random_greetings/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from signatures.views import generate_new_signature
+from signatures.views import generate_new_signature, home
 
 urlpatterns = [
-    url(r'^$', 'signatures.views.home', name='home'),
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^new_signature/(?P<optional>.*)/$', generate_new_signature, name='new_signature'),
     url(r'^new_signature/$', generate_new_signature, name='new_signature'),
